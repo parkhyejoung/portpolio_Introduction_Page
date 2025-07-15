fd120a16dfa8b0dd6b99ca767980eed9f31db13d
hyejoung <qkrgpwjd1996@gmail.com>
2025-07-15 14:57:35 +0900

    html , css 일부 완료

---
+++ scripts/index.js
@@ -0,0 +1,15 @@
+const sns = new Swiper('.disign',{
+    autoplay:{delay:0,},
+    speed:6000,
+    loop:true,
+})
+const banner = new Swiper('.banner',{
+    autoplay:{delay:0,},
+    speed:6000,
+    loop:true,
+})
+const detail = new Swiper('.detail',{
+    autoplay:{delay:0,},
+    speed:5000,
+    loop:true,
+})
